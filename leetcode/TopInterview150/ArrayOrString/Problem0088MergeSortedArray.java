1e973a099a7e1a799287f07e85b51408944de459
elliotleell <xingyuli1996@gmail.com>
2025-01-02 19:41:58 +0800

    Finish 27. Remove Element and 88. Merge Sorted Array

--- leetcode/TopInterview150/ArrayOrString/Problem0088MergeSortedArray.java
+++ leetcode/TopInterview150/ArrayOrString/Problem0088MergeSortedArray.java
@@ -1,8 +1,4 @@
 public class Solution {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-    }
-
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
     }
